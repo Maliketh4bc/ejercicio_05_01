43eadee593d27691ec6904aff513312e1cb04cb3
Marco Alvarez <malvarm1212@gmail.com>
2025-10-21 08:46:43 +0200

    finish

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        for(int i=0; i<=100; i= i+5){
+            System.out.print(i + " ");
+        }
+
     }
 }
